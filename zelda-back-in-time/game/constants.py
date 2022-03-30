104634cf3e02d91f39de1cc1dae10e6ad1959805
shauniebyrne <byr21002@byui.edu>
2022-03-30 16:27:47 -0700

    changes

--- zelda-back-in-time/game/constants.py
+++ zelda-back-in-time/game/constants.py
@@ -17,3 +17,9 @@ SCREEN_WIDTH = 960
 SCREEN_HEIGHT = 640
 
 SPRITE_SCALING_BOX = 1
+
+# Life info
+START_LIFE = 3
+
+# Key points info
+KEY_POINTS = range(0, 12)
--- zelda-back-in-time/game/directing/director.py
+++ zelda-back-in-time/game/directing/director.py
@@ -20,10 +20,8 @@ class Director(arcade.Window):
         self._cast = Cast()
 
     def setup(self):
-        """Create all Cast members"""
+        """Set up the game and create the cast."""
         self._background = Background('grass.png')
-        self._background2 = Background('ground.png')
-        self._background3 = Background('bluesky.png')
 
         self._level1 = Level('1.csv', self._cast)
         self._level2 = Level('2.csv', self._cast)
@@ -35,13 +33,19 @@ class Director(arcade.Window):
 
         self._player = Actor('player.png', [3, 3])
         self._player2 = Actor('badguy.png', [11, 8])
-        self._food = Actor('apple.png', [5, 6])
-        self._food2 = Actor('cherries.png', [8, 5])
+        self._food = Actor('apple.png', [4, 8])
+        self._food2 = Actor('cherries.png', [8, 1])
         self._key = Actor('key.png', [13, 7]) 
         self._key2 = Actor('key.png', [7, 4]) 
 
+        # Lives
+        self._life = START_LIFE
+
+        # Key count
+        self._keypoints = KEY_POINTS
+
     def on_draw(self):
-        """Draw all Cast members"""
+        """Draw everything for the game"""
         self._background.draw()
         self._player.draw()
         self._player2.draw()
@@ -51,16 +55,24 @@ class Director(arcade.Window):
         self._key.draw()
         self._key2.draw()
 
+        # Put the Lives on the screen
+        output = f"Lives: {self._life}"
+        arcade.draw_text(output, 3, 10, arcade.color.BLACK, 16)
+
+        # Put the Key Points on the screen
+        output2 = f"Keys: {self._keypoints}"
+        arcade.draw_text(output2, 8, 10, arcade.color.BLACK, 16)
+
     def on_key_press(self, key, modifiers):
-        """ Move player around gameboard.
+        """ Create what needs to happen as the hero moves around the
+        game (as keys are pressed by the player).
 
         Args:
             key: Key pressed by player.
-            modifiers: Necessary to make changes.
+            modifiers: Keys like Ctrl, Shift, etc and their controls.
         
         """
-        position = self._player.get_position()
-        
+        position = self._player.get_position()        
         x = position.get_x()
         y = position.get_y()
 
@@ -78,6 +90,12 @@ class Director(arcade.Window):
             if x <= 13:
                 x = x + 1
 
+        # Create keys to end game
+        if key == arcade.key.Q:
+            # Quit game
+            arcade.close_window()
+
+        # Swith between levels (the hero)
         if self._level_number == 1:
             if x == 0 and y == 5:
                 self._level = self._level2
@@ -98,7 +116,8 @@ class Director(arcade.Window):
                 self._level = self._level1
                 self._level_number = 1
 
-        # Check for player collision with any elements. If there is a collision, return to prevent the collision
+        # Check for player collision with any elements. 
+        # If there is a collision, return to prevent the collision
         for actor in self._cast.get_actors('elements'):
             if x == actor.get_position().get_x() and y == actor.get_position().get_y():
                 return
@@ -106,3 +125,45 @@ class Director(arcade.Window):
         position.set_x(x)
 
         position.set_y(y)
+
+    def on_update(self, delta_time: float):
+        """Updates the position and status of all actors.
+        
+        Arg:
+            delta_time (float): Time since the last update"""
+            
+        # Check if hero collides with food or keys or bad guy
+        # If food, add to Life
+        # life = self._life
+        # keypoints = self._keypoints
+        # if self._player.collides_with_point((4,8)):
+        #     life += life
+        #     self._cast.remove_actor('elements', self._food)
+
+        # elif self._player.collides_with_point((8,1)):
+        #     life += life
+        #     self._cast.remove_actor('elements', self._food2)
+
+        # # If keys, add to the key count (key points)
+        # if self._player.collides_with_point((13,7)):
+        #     keypoints += keypoints
+        #     self._cast.remove_actor('elements', self._key)
+
+        # elif self._player.collides_with_point((7,4)):
+        #     keypoints += keypoints
+        #     self._cast.remove_actor('elements', self._key2)
+
+        # # If bad guy, take away life
+        # if self._player.collides_with_sprite(self._player2):
+        #     life -= life
+
+        # Draw different backgrounds
+        if self._level_number == 1:
+            self._background = Background('grass.png')
+            self._background.draw()
+        if self._level_number == 2:
+            self._background = Background('ground.png')
+            self._background.draw()
+        elif self._level_number == 3:
+            self._background = Background('bluesky.png')
+            self._background.draw()
