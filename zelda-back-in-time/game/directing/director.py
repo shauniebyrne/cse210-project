814d7363c624cb9d86621e59e35d82537c1cf064
amo21004 <amo21004@byui.edu>
2022-04-01 18:19:31 +0530

    Finalizing for submission

--- zelda-back-in-time/game/directing/director.py
+++ zelda-back-in-time/game/directing/director.py
@@ -19,9 +19,17 @@ class Director(arcade.Window):
 
         self._cast = Cast()
 
+        self._intro = True
+
+        self._game_over = False
+
     def setup(self):
-        """Set up the game and create the cast."""
         self._background = Background('grass.png')
+
+        if self._intro or self._game_over:
+            return
+
+        """Set up the game and create the cast."""
 
         self._level1 = Level('1.csv', self._cast)
         self._level2 = Level('2.csv', self._cast)
@@ -32,7 +40,28 @@ class Director(arcade.Window):
         self._level_number = 1
 
         self._player = Actor('player.png', [3, 3])
-        self._player2 = Actor('badguy.png', [11, 8])
+
+        self._badguy_l1 = Actor('badguy.png', [11, 8])
+        self._badguy_l2 = Actor('badguy.png', [11, 8])
+        self._badguy_l3 = Actor('badguy.png', [11, 8])
+
+        self._food1_l1 = Actor('apple.png', [4, 8])
+        self._food2_l1 = Actor('cherries.png', [8, 1])
+
+        self._food1_l2 = Actor('apple.png', [4, 8])
+        self._food2_l2 = Actor('cherries.png', [8, 1])
+
+        self._food1_l3 = Actor('apple.png', [4, 8])
+        self._food2_l3 = Actor('cherries.png', [8, 1])
+
+        self._key1_l1 = Actor('key.png', [13, 7]) 
+        self._key2_l1 = Actor('key.png', [7, 4]) 
+
+        self._key1_l2 = Actor('key.png', [13, 7]) 
+        self._key2_l2 = Actor('key.png', [7, 4]) 
+
+        self._key1_l3 = Actor('key.png', [13, 7]) 
+        self._key2_l3 = Actor('key.png', [7, 4]) 
 
         # Lives
         self._life = START_LIFE
@@ -43,10 +72,64 @@ class Director(arcade.Window):
     def on_draw(self):
         """Draw everything for the game"""
         arcade.start_render()
+
+        if self._game_over:
+            self._background.draw()
+            arcade.draw_text('GAME OVER', 325, 325, arcade.color.WHITE, 20, 300, "center", "arial", True)
+            return
+        elif self._intro:
+            self._background.draw()
+
+            arcade.draw_text(GAME_NAME, 325, 400, arcade.color.WHITE, 20, 300, "center", "arial", True)
+
+            arcade.draw_text('Press [Enter] to Begin', 325, 200, arcade.color.WHITE, 20, 300, "center", "arial", True)
+            return
+
         self._background.draw()
         self._player.draw()
-        self._player2.draw()
         self._level.draw()
+
+        if self._level_number == 1 and self._badguy_l1:
+            self._badguy_l1.draw()
+
+        if self._level_number == 2 and self._badguy_l2:
+            self._badguy_l2.draw()
+
+        if self._level_number == 3 and self._badguy_l3:
+            self._badguy_l3.draw()
+
+        if self._level_number == 1 and self._food1_l1:
+            self._food1_l1.draw()
+
+        if self._level_number == 1 and self._food2_l1:
+            self._food2_l1.draw()
+
+        if self._level_number == 2 and self._food1_l2:
+            self._food1_l2.draw()
+
+        if self._level_number == 2 and self._food2_l2:
+            self._food2_l2.draw()
+
+        if self._level_number == 3 and self._food1_l3:
+            self._food1_l3.draw()
+
+        if self._level_number == 3 and self._food2_l3:
+            self._food2_l3.draw()
+
+        if self._level_number == 1 and self._key1_l1:
+            self._key1_l1.draw()
+        if self._level_number == 1 and self._key2_l1:
+            self._key2_l1.draw()
+
+        if self._level_number == 2 and self._key1_l2:
+            self._key1_l2.draw()
+        if self._level_number == 2 and self._key2_l2:
+            self._key2_l2.draw()
+
+        if self._level_number == 3 and self._key1_l3:
+            self._key1_l3.draw()
+        if self._level_number == 3 and self._key2_l3:
+            self._key2_l3.draw()
 
         # Put the Lives on the screen
         output = f"Lives:{self._life}"
@@ -65,6 +148,22 @@ class Director(arcade.Window):
             modifiers: Keys like Ctrl, Shift, etc and their controls.
         
         """
+
+        if self._game_over:
+            arcade.close_window()
+
+        # Create keys to end game
+        if key == arcade.key.Q:
+            # Quit game
+            self.game_over()
+            return
+
+        if self._intro:
+            if key == arcade.key.ENTER:
+                self._intro = False
+                self.setup()
+            return
+
         position = self._player.get_position()        
         x = position.get_x()
         y = position.get_y()
@@ -82,21 +181,6 @@ class Director(arcade.Window):
         elif key == arcade.key.RIGHT:
             if x <= 13:
                 x = x + 1
-
-        # Create key to end game
-        if key == arcade.key.Q:
-            # Quit game
-            arcade.close_window()
-
-        # Create key to play the game again
-        if key == arcade.key.Y:
-            # Run game
-            arcade.run()
-
-        # Create key keep the game ended by players choice
-        if key == arcade.key.N:
-            # Quit game
-            arcade.close_window()
 
         # Swith between levels (the hero)
         if self._level_number == 1:
@@ -118,24 +202,59 @@ class Director(arcade.Window):
             if x == 14 and y == 5:
                 self._level = self._level1
                 self._level_number = 1
-        
+
+        if self._level_number == 1 and x == 13 and y == 7:
+            self._key1_l1 = None
+            self._keypoints = self._keypoints + 1
+        elif self._level_number == 1 and x == 7 and y == 4:
+            self._key2_l1 = None
+            self._keypoints = self._keypoints + 1
+
+        if self._level_number == 2 and x == 13 and y == 7:
+            self._key1_l2 = None
+            self._keypoints = self._keypoints + 1
+        elif self._level_number == 2 and x == 7 and y == 4:
+            self._key2_l2 = None
+            self._keypoints = self._keypoints + 1
+
+        if self._level_number == 3 and x == 13 and y == 7:
+            self._key1_l3 = None
+            self._keypoints = self._keypoints + 1
+        elif self._level_number == 3 and x == 7 and y == 4:
+            self._key2_l3 = None
+            self._keypoints = self._keypoints + 1
+
+        elif x == 11 and y == 8:
+            self._life = self._life - 1
+
+        elif self._level_number == 1 and x == 4 and y == 8:
+            self._food1_l1 = None
+            self._life = self._life + 1
+        elif self._level_number == 1 and x == 8 and y == 1:
+            self._food2_l1 = None
+            self._life = self._life + 1
+
+        elif self._level_number == 2 and x == 4 and y == 8:
+            self._food1_l2 = None
+            self._life = self._life + 1
+        elif self._level_number == 2 and x == 8 and y == 1:
+            self._food2_l2 = None
+            self._life = self._life + 1
+
+        elif self._level_number == 3 and x == 4 and y == 8:
+            self._food1_l3 = None
+            self._life = self._life + 1
+        elif self._level_number == 3 and x == 8 and y == 1:
+            self._food2_l3 = None
+            self._life = self._life + 1
+
+        if self._life == 0:
+            self.game_over()
+
         # Check for player collision with any elements. 
+        # If there is a collision, return to prevent the collision
         for actor in self._cast.get_actors('elements'):
-            # Check for key or food collision, add to key points (keys) or life points (food)
-            if x == 13 and y == 7:
-                self._cast.remove_actor("elements", actor)
-                self._keypoints += 1
-            elif x == 7 and y == 4:
-                actor = None
-                self._keypoints += 1
-            elif x == 4 and y == 8:
-                actor = None
-                self._life += 1
-            elif x == 8 and y == 1:
-                actor = None
-                self._life += 1
-            # If there is a collision, return to prevent the collision
-            elif x == actor.get_position().get_x() and y == actor.get_position().get_y():
+            if x == actor.get_position().get_x() and y == actor.get_position().get_y():
                 return
 
         position.set_x(x)
@@ -147,7 +266,10 @@ class Director(arcade.Window):
         
         Arg:
             delta_time (float): Time since the last update"""
-            
+
+        if self._intro or self._game_over:
+            return
+
         # Draw different backgrounds
         if self._level_number == 1:
             self._background = Background('grass.png')
@@ -158,3 +280,7 @@ class Director(arcade.Window):
         elif self._level_number == 3:
             self._background = Background('bluesky.png')
             self._background.draw()
+
+    def game_over(self):
+        self._game_over = True
+        #arcade.close_window()
